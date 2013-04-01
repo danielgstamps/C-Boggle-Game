cd6452f6124dad49011bd7b80ac39dd087e867b2
Daniel Stamps <Daniel Stamps@localhost>
2013-04-01 14:34:34 -0400

    Started Die Class

---
+++ Die.cpp
@@ -0,0 +1,23 @@
+
+#include "Die.h"
+#include <time.h>
+
+Die::Die(void)
+{
+	this->letters = gcnew array<String^>(6);
+}
+
+Die::Die(String^ letter1, String^ letter2, String^ letter3, String^ letter4, String^ letter5, String^ letter6){
+	letters[0] = letter1;
+	letters[1] = letter2;
+	letters[2] = letter3;
+	letters[3] = letter4;
+	letters[4] = letter5;
+	letters[5] = letter6;
+}
+
+String^ Die::getRandomLetter(){
+	srand((unsigned) time(NULL));
+	int randomNumber = rand() % 6;
+	return letters[randomNumber];
+}
