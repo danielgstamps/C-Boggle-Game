c56275c6f018b82343e0d964eb9b63b7cf4d1c86
dStamps <dStamps@localhost>
2013-04-09 13:35:18 -0400

    Attempting to use Dice in GUI

--- WinCLI.cpp
+++ WinCLI.cpp
@@ -1,4 +1,5 @@
 #include "MyForm.h"
+#include <time.h>
 #include <iostream>
 using namespace std;
 using namespace Project1;
@@ -6,6 +7,7 @@ using namespace Project1;
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
+	srand((unsigned) time(NULL));
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
 
