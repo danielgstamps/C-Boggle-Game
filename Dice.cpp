f9b21359fb87afe31d1f84ab034d2d394a6c845f
dStamps <dStamps@localhost>
2013-04-09 13:04:59 -0400

    Changed Die/Dice to ref classes, started integrating into GUI.

--- Dice.cpp
+++ Dice.cpp
@@ -1,30 +1,31 @@
 #include "Dice.h"
+#include <cliext/vector>
 
 Dice::Dice(void)
 {
-	this->dice = vector<Die>(16);
+	this->dice = cliext::vector<Die^>(16);
 	this->populateDiceList();
 }
 
 void Dice::populateDiceList(){
-	this->dice[0] = Die("R", "I", "F", "O", "B", "X");
-	this->dice[1] = Die("I", "F", "E", "H", "E", "Y");
-	this->dice[2] = Die("D", "E", "N", "O", "W", "S");
-	this->dice[3] = Die("U", "T", "O", "K", "N", "D");
-	this->dice[4] = Die("H", "M", "S", "R", "A", "O");
-	this->dice[5] = Die("L", "U", "P", "E", "T", "S");
-	this->dice[6] = Die("A", "C", "I", "T", "O", "A");
-	this->dice[7] = Die("Y", "L", "G", "K", "U", "E");
-	this->dice[8] = Die("Qu", "B", "M", "J", "O", "A");
-	this->dice[9] = Die("E", "H", "I", "S", "P", "N");
-	this->dice[10] = Die("V", "E", "T", "I", "G", "N");
-	this->dice[11] = Die("B", "A", "L", "I", "Y", "T");
-	this->dice[12] = Die("E", "Z", "A", "V", "N", "D");
-	this->dice[13] = Die("R", "A", "L", "E", "S", "C");
-	this->dice[14] = Die("U", "W", "I", "L", "R", "G");
-	this->dice[15] = Die("P", "A", "C", "E", "M", "D");
+	this->dice.push_back(gcnew Die("R", "I", "F", "O", "B", "X"));
+	this->dice.push_back(gcnew Die("I", "F", "E", "H", "E", "Y"));
+	this->dice.push_back(gcnew Die("D", "E", "N", "O", "W", "S"));
+	this->dice.push_back(gcnew Die("U", "T", "O", "K", "N", "D"));
+	this->dice.push_back(gcnew Die("H", "M", "S", "R", "A", "O"));
+	this->dice.push_back(gcnew Die("L", "U", "P", "E", "T", "S"));
+	this->dice.push_back(gcnew Die("A", "C", "I", "T", "O", "A"));
+	this->dice.push_back(gcnew Die("Y", "L", "G", "K", "U", "E"));
+	this->dice.push_back(gcnew Die("Qu", "B", "M", "J", "O", "A"));
+	this->dice.push_back(gcnew Die("E", "H", "I", "S", "P", "N"));
+	this->dice.push_back(gcnew Die("V", "E", "T", "I", "G", "N"));
+	this->dice.push_back(gcnew Die("B", "A", "L", "I", "Y", "T"));
+	this->dice.push_back(gcnew Die("E", "Z", "A", "V", "N", "D"));
+	this->dice.push_back(gcnew Die("R", "A", "L", "E", "S", "C"));
+	this->dice.push_back(gcnew Die("U", "W", "I", "L", "R", "G"));
+	this->dice.push_back(gcnew Die("P", "A", "C", "E", "M", "D"));
 }
 
-Die Dice::getElementAt(int index){
-	return this->dice[index];
+Die^ Dice::getElementAt(int index){
+	return this->dice.at(index);
 }
