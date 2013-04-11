599e81ce3ab16d9f1d5e977aa87d201c7616aeee
dstamps1 <dstamps1@localhost>
2013-04-10 22:25:22 -0400

    Refactored GUI

--- WinCLI.cpp
+++ WinCLI.cpp
@@ -10,7 +10,6 @@ int main(array<System::String ^> ^args)
 	srand((unsigned) time(NULL));
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
-
 	Application::Run(gcnew MyForm());
 	return 0;
 }
