1970cb200702a7fe1eced46e535c156a2e95ca36
dstamps1 <dstamps1@localhost>
2013-04-14 22:58:47 -0400

    finished Timer implementation.

---
+++ Dictionary.cpp
@@ -0,0 +1,48 @@
+
+#include "Dictionary.h"
+
+
+
+Dictionary::Dictionary(){
+	this->set = unordered_set<string>();
+}
+
+void Dictionary::loadDictionary(){
+	string line;
+	ifstream myfile ("dictionary.txt");
+
+	if (myfile.is_open()){
+
+		while ( myfile.good() ){
+			getline (myfile,line);
+			this->set.insert(line);
+		}
+
+    myfile.close();
+	}
+}
+
+bool Dictionary::wordExist(String^ word){
+	System::IntPtr pointer = Marshal::StringToHGlobalAnsi(word);
+	char* charPointer = reinterpret_cast<char*>(pointer.ToPointer());
+    std::string stdString(charPointer, word->Length);
+    Marshal::FreeHGlobal(pointer);
+	return this->set.find(stdString)!= this->set.end();
+}
+
+int Dictionary::points(String^ word){
+	int point = 0;
+	if(word->Length==3||word->Length==4){
+		point =1;
+	}else if(word->Length==5){
+		point = 2;
+	}else if(word->Length==6){
+		point = 3;
+	}else if(word->Length==7){
+		point =5;
+	}else if(word->Length>=8){
+		point =11;
+	}
+
+	return point;
+}
---
+++ MyForm.h
@@ -0,0 +1,117 @@
+#pragma once
+#include "Dice.h"
+#include "Dictionary.h"
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+	MyForm();
+
+	protected:
+	~MyForm();
+
+	private: 
+	String^ userWordChoice;
+	Dice^ dice;
+	array<Button^>^ selectedButtons;	
+	array<String^>^ guessedWords;
+	int buttonCounter;
+	int wordCounter;
+	int minutesRemaining;
+	private: System::Windows::Forms::Button^  newGameButton;
+	private: System::Windows::Forms::Button^  highScoresButton;
+			 int secondsRemaining;
+ 
+	void addGuessedWord();
+	void InitializeComponent();
+	void initializeDice();
+	void cornerDie(Button^ itSelf,Button^ sideOne,Button^ sideTwo, Button^ corner);
+	void insideEdgeDie(Button^ itSelf,Button^ sideOne,Button^ sideTwo,Button^ sideThree, Button^ cornerOne, Button^ cornerTwo);
+	void insideDie(Button^ itSelf,Button^ sideOne,Button^ sideTwo,Button^ sideThree, Button^ sideFour, Button^ cornerOne, Button^ cornerTwo, Button^ cornerThree, Button^ cornerFour);
+	void disableAllButtons();
+	void disableButtonsClicked();
+	void enableButtons();
+	void endGame();
+	void resetTimer();
+
+	// ----- FORMS ------ //
+	System::Windows::Forms::Button^  die0;
+	System::Windows::Forms::Button^  die1;
+	System::Windows::Forms::Button^  die2;
+	System::Windows::Forms::Button^  die3;
+	System::Windows::Forms::Button^  die4;
+	System::Windows::Forms::Button^  die5;
+	System::Windows::Forms::Button^  die6;
+	System::Windows::Forms::Button^  die7;
+	System::Windows::Forms::Button^  die8;
+	System::Windows::Forms::Button^  die9;
+	System::Windows::Forms::Button^  die10;
+	System::Windows::Forms::Button^  die11;
+	System::Windows::Forms::Button^  die12;
+	System::Windows::Forms::Button^  die13;
+	System::Windows::Forms::Button^  die14;
+	System::Windows::Forms::Button^  die15;
+	System::Windows::Forms::Button^  submitButton;
+	System::Windows::Forms::Timer^  countdownTimer;
+	System::Windows::Forms::Label^  titleLabel;
+	System::Windows::Forms::Label^  yourWordLabel;
+	System::Windows::Forms::Label^  userWordLabel;
+
+	System::Windows::Forms::Label^  timeLabel;
+	System::Windows::Forms::Label^  secondsLeftLabel;
+	System::Windows::Forms::Label^  timerColonLabel;
+	System::Windows::Forms::Label^  minutesLeftLabel;
+
+	System::Windows::Forms::Panel^  dicePanel;
+	System::Windows::Forms::Panel^  timerPanel;
+	System::Windows::Forms::Panel^  wordGuessPanel;
+	System::Windows::Forms::MenuStrip^  menuStrip1;
+	System::Windows::Forms::RichTextBox^  userGuessesBox;
+	System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  startToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  stopToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  highScoresToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  infoToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  howToToolStripMenuItem;
+	System::Windows::Forms::ToolStripMenuItem^  infoToolStripMenuItem1;		
+	System::ComponentModel::IContainer^  components;
+
+
+	// ----- ACTION LISTENERS ------ //
+	System::Void MyForm::die0_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die1_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die2_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die3_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die4_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die5_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die6_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die7_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die8_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die9_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die10_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die11_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die12_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die13_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die14_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::die15_Click(System::Object^  sender, System::EventArgs^  e);
+	System::Void MyForm::submitButton_Click(System::Object^  sender, System::EventArgs^  e);	
+	System::Void MyForm::countdownTimer_Tick(System::Object^  sender, System::EventArgs^  e); 
+	System::Void MyForm::newGameButton_Click(System::Object^  sender, System::EventArgs^  e);	
+	};
+
+#pragma region Windows Form Designer generated code
+#pragma endregion
+}
