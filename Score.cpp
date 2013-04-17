abd4378b7754cf4b5dcb018bb8302e4e7b09e76d
BryanPatterson <BryanPatterson@localhost>
2013-04-17 06:44:22 -0400

    All methods are formated and commented

--- Score.cpp
+++ Score.cpp
@@ -1,13 +1,28 @@
 #include "Score.h"
-
+/*
+Constuctor for Score
+Predcondition: none
+@param name player name
+@param score player score
+*/
 Score::Score(String^ name, int score){
 	this->name = name;
 	this->score = score;
 }
-
+/*
+This method gets player name
+Predcondition: none
+@return player name
+*/
 String^ Score::getName(){
 	return this->name;
 }
+
+/*
+This method gets player score
+Predcondition: none
+@retun int of player score.
+*/
 int Score::getScore(){
 	return this->score;
 }
